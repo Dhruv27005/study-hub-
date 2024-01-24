e1ccd09c9582d7c38c7df428f8382f8fed721681
Dhruv27005 <153294549+Dhruv27005@users.noreply.github.com>
2024-01-24 12:55:55 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,26 +1,26 @@
 function showChat(){
-    alert("please sign up to join the  chat");
+    alert("please login to join the  chat");
   }
   function showUpload(){
-    alert("please sign up to upload files");
+    alert("please login to upload files");
   }
   function viewFiles(){
-    alert("please sign up to view the uploaded files");
+    alert("please login to view the uploaded files");
   }
   function publishBlog(){
-    alert("please sign up to upload Blogs");
+    alert("please login to upload Blogs");
   }
   function viewBlog(){
-    alert("please sign up to view the uploaded Blogs");
+    alert("please login to view the uploaded Blogs");
   }
   document.getElementById("view_blog").addEventListener("click",viewBlog);
 
   document.getElementById("publish_blog").addEventListener("click",publishBlog);
   function sem1(){
-    alert("please sign up to view/upload files");
+    alert("please login to view/upload files");
   }
   function sem2(){
-    alert("please sign up to view/upload files");
+    alert("please login to view/upload files");
   }
   document.getElementById("sem1").addEventListener("click",sem1);
   document.getElementById("sem2").addEventListener("click",sem2);
@@ -28,11 +28,11 @@ document.getElementById("semR1").addEventListener("click",semR1);
   document.getElementById("semR2").addEventListener("click",semR2);
   document.getElementById("booksR").addEventListener("click",booksR);
   function semR1(){
-    alert("please sign up to view sem 1 study materials");
+    alert("please login to view sem 1 study materials");
   }
   function semR2(){
-    alert("please sign up to view sem 2 study materials");
+    alert("please login to view sem 2 study materials");
   }
   function booksR(){
-    alert("please sign up to view reference books");
+    alert("please login to view reference books");
   }
